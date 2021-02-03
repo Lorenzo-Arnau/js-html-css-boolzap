10547902f8711e82ca6ed25ec21e6dc1da80abd6
Lorenzo-Arnau <skyline03@hotmail.it>
2021-02-03 17:16:17 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,111 +1,111 @@
-var todayTime = new Date().toLocaleString();
-var app = new Vue({
- el: '#root',
- data:{
-   lastMessageBool : true,
-   popIndex : false,
-   lastMessageRead: '',
-   searchResult: '',
-   notify : false,
-   notifyStatus:'Attiva',
-   sentTime: todayTime,
-   selected : 'Michele',
-   selectedPic :'_1',
-   lastAccessDate :'10/01/2020',
-   lastAccessTime:'16:15',
-   currentIndex:0,
-   newMessage:'',
-   contacts: [
+let todayTime = new Date().toLocaleString();
+let app = new Vue({
+  el: '#root',
+  data:{
+    lastMessageBool : true,
+    popIndex : false,
+    lastMessageRead: '',
+    searchResult: '',
+    notify : false,
+    notifyStatus:'Attiva',
+    sentTime: todayTime,
+    selected : 'Michele',
+    selectedPic :'_1',
+    lastAccessDate :'10/01/2020',
+    lastAccessTime:'16:15',
+    currentIndex:0,
+    newMessage:'',
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages: [
           {
-          name: 'Michele',
-          avatar: '_1',
-          visible: true,
-          messages: [
-           {
-             date: '10/01/2020 15:30:55',
-             text: 'Hai portato a spasso il cane?',
-             status: 'sent'
-           },
-           {
-             date: '10/01/2020 15:50:00',
-             text: 'Ricordati di dargli da mangiare',
-             status: 'sent'
-           },
-           {
-             date: '10/01/2020 16:15:22',
-             text: 'Tutto fatto!',
-             status: 'received'
-           }
-          ],
+            date: '10/01/2020 15:30:55',
+            text: 'Hai portato a spasso il cane?',
+            status: 'sent'
           },
           {
-          name: 'Fabio',
-          avatar: '_2',
-          visible: true,
-          messages: [
-           {
-             date: '20/03/2020 16:30:00',
-             text: 'Ciao come stai?',
-             status: 'sent'
-           },
-           {
-             date: '20/03/2020 16:30:55',
-             text: 'Bene grazie! Stasera ci vediamo?',
-             status: 'received'
-           },
-           {
-             date: '20/03/2020 16:35:00',
-             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-             status: 'sent'
-           }
-          ],
+            date: '10/01/2020 15:50:00',
+            text: 'Ricordati di dargli da mangiare',
+            status: 'sent'
           },
           {
-          name: 'Samuele',
-          avatar: '_3',
-          visible: true,
-          messages: [
-           {
-             date: '28/03/2020 10:10:40',
-             text: 'La Marianna va in campagna',
-             status: 'received'
-           },
-           {
-             date: '28/03/2020 10:20:10',
-             text: 'Sicuro di non aver sbagliato chat?',
-             status: 'sent'
-           },
-           {
-             date: '28/03/2020 16:15:22',
-             text: 'Ah scusa!',
-             status: 'received'
-           }
-          ],
+            date: '10/01/2020 16:15:22',
+            text: 'Tutto fatto!',
+            status: 'received'
+          }
+        ],
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages: [
+          {
+            date: '20/03/2020 16:30:00',
+            text: 'Ciao come stai?',
+            status: 'sent'
           },
           {
-          name: 'Luisa',
-          avatar: '_4',
-          visible: true,
-          messages: [
-           {
-             date: '10/01/2020 15:30:55',
-             text: 'Lo sai che ha aperto una nuova pizzeria?',
-             status: 'sent'
-           },
-           {
-             date: '10/01/2020 15:50:00',
-             text: 'Si, ma preferirei andare al cinema',
-             status: 'received'
-           }
-          ],
+            date: '20/03/2020 16:30:55',
+            text: 'Bene grazie! Stasera ci vediamo?',
+            status: 'received'
           },
-          ]
- },
- methods:{
+          {
+            date: '20/03/2020 16:35:00',
+            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+            status: 'sent'
+          }
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true,
+        messages: [
+          {
+            date: '28/03/2020 10:10:40',
+            text: 'La Marianna va in campagna',
+            status: 'received'
+          },
+          {
+            date: '28/03/2020 10:20:10',
+            text: 'Sicuro di non aver sbagliato chat?',
+            status: 'sent'
+          },
+          {
+            date: '28/03/2020 16:15:22',
+            text: 'Ah scusa!',
+            status: 'received'
+          }
+        ],
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+        visible: true,
+        messages: [
+          {
+            date: '10/01/2020 15:30:55',
+            text: 'Lo sai che ha aperto una nuova pizzeria?',
+            status: 'sent'
+          },
+          {
+            date: '10/01/2020 15:50:00',
+            text: 'Si, ma preferirei andare al cinema',
+            status: 'received'
+          }
+        ],
+      },
+    ]
+  },
+  methods:{
     selectedUser : function(i) {
-      var lastMessage = i.messages[i.messages.length - 1].date;
-      var dateTimeArray =lastMessage.split(' ');
-      var createdDate = dateTimeArray[1].split(':',2);
+      let lastMessage = i.messages[i.messages.length - 1].date;
+      let dateTimeArray =lastMessage.split(' ');
+      let createdDate = dateTimeArray[1].split(':',2);
       this.selected = i.name;
       this.currentIndex = this.contacts.indexOf(i);
       this.selectedPic = i.avatar;
@@ -114,85 +114,76 @@ var app = new Vue({
     },
     addMessage : function(message,status) {
       this.contacts[this.currentIndex].messages = this.contacts[this.currentIndex].messages.concat(
-      {
-        date: this.sentTime,
-        text: message,
-        status: status,
-      })
-      this.lastMessageRead = this.contacts[this.currentIndex].messages[this.contacts[this.currentIndex].messages.length - 1].text;
-      console.log(this.lastMessageRead);
-      console.log(this.sentTime);
-      this.newMessage = '';
-    },
-    receivedMessage : function(){
-      let that = this;
-      var lastTimeChange = this.sentTime.split(' ')[1].split(':',2);
-      setTimeout(function() {
-        switch (that.lastMessageRead.toLowerCase()) {
-          case 'ciao':
+        {
+          date: this.sentTime,
+          text: message,
+          status: status,
+        })
+        this.lastMessageRead = this.contacts[this.currentIndex].messages[this.contacts[this.currentIndex].messages.length - 1].text;
+        this.newMessage = '';
+      },
+      receivedMessage : function(){
+        let that = this;
+        let lastTimeChange = this.sentTime.split(' ')[1].split(':',2);
+        setTimeout(function() {
+          switch (that.lastMessageRead.toLowerCase()) {
+            case 'ciao':
             that.addMessage('ciao a te simpatico utente!','received')
             break;
-          case 'come stai?':
+            case 'come stai?':
             that.addMessage('alla grande! Devo scappare ci sentiamo dopo!','received')
             break;
-          case 'easter egg':
+            case 'easter egg':
             that.addMessage('Lo hai trovato finalmente!','received')
             break;
-          default:
+            default:
             that.addMessage('non posso risponderti adesso..ti scrivo dopo!','received')
-        }
-        that.lastAccessTime = lastTimeChange[0] +':'+ lastTimeChange[1];
-      }, 1500);
-    },
-    createDate : function(currentIndex,idx){
-        var createdDate = this.contacts[currentIndex].messages[idx].date.split(' ')[1].split(':',2);
+          }
+          that.lastAccessTime = lastTimeChange[0] +':'+ lastTimeChange[1];
+        }, 1500);
+      },
+      createDate : function(currentIndex,idx){
+        let createdDate = this.contacts[currentIndex].messages[idx].date.split(' ')[1].split(':',2);
         return createdDate[0] +':'+ createdDate[1];
-    },
-    lastMessage : function(index){
-      let thisText = this.contacts[index].messages[this.contacts[index].messages.length - 1];
+      },
+      lastMessage : function(index){
+        let thisText = this.contacts[index].messages[this.contacts[index].messages.length - 1];
         return thisText.text;
+      },
+      changeNotify : function(){
+        if (this.notify === false) {
+          this.notify = true;
+          this.notifyStatus='Disattiva';
+        }else {
+          this.notify = false;
+          this.notifyStatus='Attiva';
+        }
+        return this.notify
+      },
+      search : function(index){
+        if ( index.name.toLowerCase().startsWith(this.searchResult.toLowerCase()) && this.searchResult != '') {
+          return true
+        }
+      },
+      popUpOpen :function (index) {
+        this.popIndex = index;
+      },
+      popUpClose : function() {
+        this.popIndex = false;
+      },
+      popUpVisibilityClass: function(index) {
+        if(index === this.popIndex) {
+          return 'pop-up active'
+        }
+        return 'pop-up'
+      },
+      deleteMessage:function(currentIndex,idx){
+        let messageList = this.contacts[currentIndex].messages;
+        Vue.delete(messageList,idx);
+        if (messageList.length <= 0) {
+          this.lastMessageBool = false;
+        }
+      },
     },
-    changeNotify : function(){
-      if (this.notify === false) {
-        this.notify = true;
-        this.notifyStatus='Disattiva';
-      }else {
-        this.notify = false;
-        this.notifyStatus='Attiva';
-      }
-      return this.notify
-    },
-   search : function(index){
-     if ( index.name.toLowerCase().startsWith(this.searchResult.toLowerCase()) && this.searchResult != '') {
-       return true
-     }
-   },
-   popUpOpen :function (index) {
-     this.popIndex = index;
-   },
-   popUpClose : function() {
-      this.popIndex = false;
-   },
-   popUpVisibilityClass: function(index) {
-     if(index === this.popIndex) {
-       return 'pop-up active'
-     }
-      return 'pop-up'
-   },
-   deleteMessage:function(currentIndex,idx){
-     var messageList = this.contacts[currentIndex].messages;
-<<<<<<< HEAD
-     console.log(idx);
-=======
-     console.log(messageList);
-     console.log(idx);
-     console.log(this.lastMessageBool);
->>>>>>> master
-     Vue.delete(messageList,idx);
-     if (messageList.length <= 0) {
-       this.lastMessageBool = false;
-     }
-   },
- },
-});
-Vue.config.devtools = true;
+  });
+  Vue.config.devtools = true;
